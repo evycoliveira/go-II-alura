5aabbbd7d7a9241b9df35e1dc109041ae58fa108
Evelyn Cristini Oliveira <evelyn.oliveira@mercadolivre.com>
2023-01-17 11:10:22 -0300

    Add withdraw method

--- main.go
+++ main.go
@@ -2,62 +2,32 @@ package main
 
 import "fmt"
 
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
 func main() {
-	type ContaCorrente struct {
-		titular       string
-		numeroAgencia int
-		numeroConta   int
-		saldo         float64
-	}
 
 	fmt.Println(ContaCorrente{})
+	contaDaSilvia := ContaCorrente{}
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500
 
-	// Primeira forma de atribuição de valores
-	contaDaEvelyn := ContaCorrente{
-		titular:       "Evelyn",
-		numeroAgencia: 589,
-		numeroConta:   123456,
-		saldo:         125.5,
+	fmt.Println(contaDaSilvia.saldo)
+	fmt.Println(contaDaSilvia.Sacar(200))
+	fmt.Println(contaDaSilvia.saldo)
+
+}
+
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso!"
+	} else {
+		return "Saldo insuficiente!"
 	}
-
-	contaDaEvelyn2 := ContaCorrente{
-		titular:       "Evelyn",
-		numeroAgencia: 589,
-		numeroConta:   123456,
-		saldo:         125.5,
-	}
-
-	// Segunda forma de atribuição de todos os valores
-	contaDaLucilene := ContaCorrente{"Lucilene", 222, 111222, 200}
-	contaDaLucilene2 := ContaCorrente{"Lucilene", 222, 111222, 200}
-	// Terceira forma com passagem parcial de valores
-	contaDoGuilherme := ContaCorrente{titular: "Guilherme", saldo: 125.5}
-
-	// True
-	fmt.Println(contaDaEvelyn == contaDaEvelyn2)
-	// True
-	fmt.Println(contaDaLucilene == contaDaLucilene2)
-	fmt.Println(contaDaEvelyn)
-	fmt.Println(contaDaLucilene)
-	fmt.Println(contaDoGuilherme)
-
-	// É necessaria tornar um ponteiro para que na linha do new, a referência não tenha erro
-	var contaDaCristiane *ContaCorrente
-	contaDaCristiane = new(ContaCorrente)
-	contaDaCristiane.titular = "Cristiane"
-	contaDaCristiane.saldo = 500
-
-	var contaDaCristiane2 *ContaCorrente
-	contaDaCristiane2 = new(ContaCorrente)
-	contaDaCristiane.titular = "Cristiane"
-	contaDaCristiane.saldo = 500
-
-	// Resultado será o endereço de memoria e os valores atribuídos
-	fmt.Println(contaDaCristiane)
-	// Resultado será o endereço os valores atribuídos
-	fmt.Println(*contaDaCristiane)
-	// False: mesmo sendo os mesmos valores, por conta dos diferentes endereços de memoria
-	fmt.Println(contaDaCristiane == contaDaCristiane2)
-	// Endereços de memória diferentes
-	fmt.Println(*contaDaCristiane == *contaDaCristiane2)
 }
