dab14565922ad386c42b2b1c3304274cbf5d8f84
Evelyn Cristini Oliveira <evelyn.oliveira@mercadolivre.com>
2023-01-17 09:40:24 -0300

    Add comparing type of variables and pointers

--- main.go
+++ main.go
@@ -20,11 +20,23 @@ func main() {
 		saldo:         125.5,
 	}
 
+	contaDaEvelyn2 := ContaCorrente{
+		titular:       "Evelyn",
+		numeroAgencia: 589,
+		numeroConta:   123456,
+		saldo:         125.5,
+	}
+
 	// Segunda forma de atribuição de todos os valores
 	contaDaLucilene := ContaCorrente{"Lucilene", 222, 111222, 200}
+	contaDaLucilene2 := ContaCorrente{"Lucilene", 222, 111222, 200}
 	// Terceira forma com passagem parcial de valores
 	contaDoGuilherme := ContaCorrente{titular: "Guilherme", saldo: 125.5}
 
+	// True
+	fmt.Println(contaDaEvelyn == contaDaEvelyn2)
+	// True
+	fmt.Println(contaDaLucilene == contaDaLucilene2)
 	fmt.Println(contaDaEvelyn)
 	fmt.Println(contaDaLucilene)
 	fmt.Println(contaDoGuilherme)
@@ -35,8 +47,17 @@ func main() {
 	contaDaCristiane.titular = "Cristiane"
 	contaDaCristiane.saldo = 500
 
+	var contaDaCristiane2 *ContaCorrente
+	contaDaCristiane2 = new(ContaCorrente)
+	contaDaCristiane.titular = "Cristiane"
+	contaDaCristiane.saldo = 500
+
 	// Resultado será o endereço de memoria e os valores atribuídos
 	fmt.Println(contaDaCristiane)
 	// Resultado será o endereço os valores atribuídos
 	fmt.Println(*contaDaCristiane)
+	// False: mesmo sendo os mesmos valores, por conta dos diferentes endereços de memoria
+	fmt.Println(contaDaCristiane == contaDaCristiane2)
+	// Endereços de memória diferentes
+	fmt.Println(*contaDaCristiane == *contaDaCristiane2)
 }
