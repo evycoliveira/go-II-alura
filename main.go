dc9f6c81a7fa37d6caf68eb24f274c84374a4715
Evelyn Cristini Oliveira <evelyn.oliveira@mercadolivre.com>
2023-01-17 09:27:34 -0300

    Refactor data structure using struct

--- main.go
+++ main.go
@@ -11,4 +11,18 @@ func main() {
 	}
 
 	fmt.Println(ContaCorrente{})
+
+	contaDaEvelyn := ContaCorrente{
+		titular:       "Evelyn",
+		numeroAgencia: 589,
+		numeroConta:   123456,
+		saldo:         125.5,
+	}
+
+	contaDaLucilene := ContaCorrente{"Lucilene", 222, 111222, 200}
+	contaDoGuilherme := ContaCorrente{titular: "Guilherme", saldo: 125.5}
+
+	fmt.Println(contaDaEvelyn)
+	fmt.Println(contaDaLucilene)
+	fmt.Println(contaDoGuilherme)
 }
