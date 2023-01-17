2e224fb6bba63c16202c1cb64b1c0d9ee424b76f
Evelyn Cristini Oliveira <evelyn.oliveira@mercadolivre.com>
2023-01-17 09:25:11 -0300

    Add struct checking account

--- main.go
+++ main.go
@@ -3,16 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var titular string = "Evelyn"
-	var numeroAgencia int = 589
-	var numeroConta int = 123456
-	var saldo float64 = 125.50
+	type ContaCorrente struct {
+		titular       string
+		numeroAgencia int
+		numeroConta   int
+		saldo         float64
+	}
 
-	var titular2 string = "Luciene"
-	var numeroAgencia2 int = 555
-	var numeroConta2 int = 111333
-	var saldo2 float64 = 200
-
-	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
-	fmt.Println(titular2, numeroAgencia2, numeroConta2, saldo2)
+	fmt.Println(ContaCorrente{})
 }
