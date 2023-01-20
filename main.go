f42799a7ddcccb1a07226d69ee817fed473b260a
Evelyn Cristini Oliveira <evelyn.oliveira@mercadolivre.com>
2023-01-20 18:30:02 -0300

    Add visibility and getBalance function

--- main.go
+++ main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/alura/banco/clientes"
 	"github.com/alura/banco/contas"
 )
 
 func main() {
-	clienteBruno := clientes.Titular{"Bruno", "123.222.333-88", "Desenvolvedor Go"}
+	contaExemplo := contas.ContaCorrente{}
+	contaExemplo.Depositar(100)
 
-	contaDoBruno := contas.ContaCorrente{clienteBruno, 123, 123456, 100}
-	fmt.Println(contaDoBruno)
+	fmt.Println(contaExemplo.ObterSaldo())
 }
