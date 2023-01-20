523e9eae0dd6b9968b553ea64cf30985c50d70f4
Evelyn Cristini Oliveira <evelyn.oliveira@mercadolivre.com>
2023-01-20 11:45:59 -0300

    Refactor second form on inline types

--- main.go
+++ main.go
@@ -8,15 +8,8 @@ import (
 )
 
 func main() {
-	contaDoBruno := contas.ContaCorrente{Titular: clientes.Titular{
-		Nome:      "Bruno",
-		CPF:       "123.222.444-77",
-		Profissao: "Desenvolvedor",
-	},
-		NumeroAgencia: 123,
-		NumeroConta:   123456,
-		Saldo:         100,
-	}
+	clienteBruno := clientes.Titular{"Bruno", "123.222.333-88", "Desenvolvedor Go"}
 
+	contaDoBruno := contas.ContaCorrente{clienteBruno, 123, 123456, 100}
 	fmt.Println(contaDoBruno)
 }
